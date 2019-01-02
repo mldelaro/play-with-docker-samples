951a2587cb09a70e18809a63e7aa970b35696143
mldelaro <mldelaro@asu.edu>
2019-01-02 01:03:03 -0700

    quickfix Dockerfile

--- pwd-simple-node-web/main.js
+++ pwd-simple-node-web/main.js
@@ -1,6 +1,13 @@
 var http = require('http')
 
-var server = http.createServer((request, response) -> {
+var server = http.createServer((request, response) => {
   response.writeHead(200)
   response.end('Hello NodeJS!')
+});
+
+server.listen(8080, (err) => {
+	if (err) {
+		return console.log('something bad happened', err)
+	}
+	console.log('server is listening on 8080')
 });
